21c93574509777d0ec0f6d8b9a6146bf01261c39
colocolo317 <colocolo317@gmail.com>
2024-03-26 14:59:45 +0800

    update fatfs R0.11 to R0.15.

--- third_party/FatFs/port/user_diskio_spi.c
+++ third_party/FatFs/port/user_diskio_spi.c
@@ -332,7 +332,7 @@ static void rcvr_spi_multi ( BYTE *buff,		/* Pointer to data buffer */
 	}
 #else
 
-	static uint8_t dummy[_MIN_SS] ;
+	static uint8_t dummy[FF_MIN_SS] ;
   osSemaphoreAcquire(sdcard_spi.lock, osWaitForever);
 
   //FIXME. we should use sl_si91x_gspi_receive_data()
@@ -358,7 +358,7 @@ void xmit_spi_multi (
 )
 {
 
-  static uint8_t dummy[_MIN_SS] ;
+  static uint8_t dummy[FF_MIN_SS] ;
   //printf("xmit_spi_multi tx len%d\r\n", btx);
 #if 0
 	for(UINT i=0; i<btx; i++) {
